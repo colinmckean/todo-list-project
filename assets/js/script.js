a888ab4f5c826779fe445d104715e8aee1371ee4
Colin McKean <ezikill@gmail.com>
2016-10-04 15:04:32 +0100

    add final style

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ $("input[type='text']").on("keypress", function(e){
     if(e.which === 13){
         var todoText = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X </span>" + todoText + "</li>")
+        $("ul").append('<li><span><i class="fa fa-trash" aria-hidden="true"></i> </span> ' + todoText + "</li>")
     }
 })
 ////selects list items and attaches click listener
